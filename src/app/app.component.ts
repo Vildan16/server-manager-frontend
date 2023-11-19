756b999dc8ee083af7e252adfb74432788c98d0f
Vildan16 <vildan.asckarov@gmail.com>
2023-11-19 14:09:46 +0300

    services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterOutlet} from '@angular/router';
+import {HttpClientModule} from "@angular/common/http";
+import {ServerService} from "./service/server.service";
+import {map, Observable, of, startWith} from "rxjs";
+import {AppState} from "./interface/app-state";
+import {CustomResponse} from "./interface/custom-response";
+import {DataState} from "./enum/data-state.enum";
+import {catchError} from "rxjs/operators";
+import {error} from "@angular/compiler-cli/src/transformers/util";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'serverapp';
+export class AppComponent implements OnInit{
+  appState$: Observable<AppState<CustomResponse>>;
+  constructor(private serverService: ServerService) {
+  }
+
+  ngOnInit(): void {
+    this.appState$ = this.serverService.servers$
+      .pipe(
+        map(response => {
+          return { dataState: DataState.LOADED_STATE, appData: response}
+        }),
+        startWith({ dataState: DataState.LOADING_STATE }),
+        catchError((error: string) => {
+          return of({ dataState: DataState.ERROR_STATE, error })
+        })
+      )
+  }
 }
